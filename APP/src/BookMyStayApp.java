c0be883fa02368ed3a446416b20238c62e6efa79
supradeepp <supradeep.p4@gmail.com>
2026-04-07 09:00:50 +0530

    Initial commit - Use Case 1 Book My Stay App

---
+++ APP/src/BookMyStayApp.java
@@ -0,0 +1,22 @@
+
+/**
+ * Book My Stay App - Hotel Booking Management System
+ * This class represents the entry point of the application
+ * and prints the welcome message when the program starts.
+ *
+ * @author Lalitya
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println("      Welcome to Book My Stay");
+        System.out.println("   Hotel Booking Management App");
+        System.out.println("           Version 1.0");
+        System.out.println("===================================");
+
+    }
+}
